3eae107572cc22c99ff659e95defa84fc8c20d9a
Judea Dumont <31213205+JudeaDumont@users.noreply.github.com>
2017-08-30 18:27:32 -0600

    Successfull Post
    Using the free web server hoster 000WebHost, I was able to perform a successful post with java script using jquery, ajax, and php. This is the beginning of a seemingly old school means of web development. However imperative to understand. I may continue this development, I will also start the development using a java servlet and google app engine.

--- handlecredentials.php
+++ handlecredentials.php
@@ -1,10 +1,3 @@
 <?php
-if (!empty($_POST['na']) && !empty($_POST['pw']))
-{
-    echo "<strong>Post received.</strong> <br/> <br/> <strong>Name:</strong> " . $_POST['name'] . "<br/><strong>Info:</strong> ";
-}
-else
-{
-    echo "Post not received.";
-}
+echo $_POST['name'];
 ?>
--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-$('.message a').click(function() {
-  $('form').animate({
-    height: "toggle",
-    opacity: "toggle"
-  }, "slow");
-});
-
 function login() {
-  alert("logging in")
-  alert(document.getElementById("4").value + " + " + document.getElementById("5").value)
+  alert(document.getElementById("4").value + " + " + document.getElementById("5").value);
   $.ajax({
-    url: "http://jude-the-software-dev.000webhostapp.com/handlecredentials.php",
-    type: post,
-    data: {
-      na: document.getElementById("4").value,
-      pw: document.getElementById("5").value
-    },
-    success: function(data) {
-      alert(data); //response from the page.php
+    data: 'name=' + document.getElementById("4").value,
+    url: 'handlecredentials.php',
+    method: 'POST', // or GET
+    success: function(msg) {
+        alert(msg);
     }
-  });
+});
 };
